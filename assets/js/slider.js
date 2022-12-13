f3bf7570ac504cf793d94280b5b60d8714fa9e27
RaioDigital <raiodigitalweb@gmail.com>
2022-12-13 19:05:22 -0300

    testes de slides

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var owl = $('.cls_hmslder');
+   owl.owlCarousel({
+       items:1,
+       loop:true,
+       autoplay:true,
+       animateIn: 'slideInDown'
+   });
+    
+});
