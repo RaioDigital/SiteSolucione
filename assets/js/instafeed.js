ee09694731496893a0633822fc4c373077443269
RaioDigital <raiodigitalweb@gmail.com>
2022-12-14 10:21:26 -0300

    Alterações ajustadas

--- assets/js/instafeed.js
+++ assets/js/instafeed.js
@@ -1,5 +1,5 @@
 var feed = new Instafeed({
-    accessToken: '',
+    accessToken: 'IGQVJWbTl1ZAEhyUlg5V3hrOWhLRkFLb0tlV0RjYi10QUpmcUhQTHVLdkhUWm1TTmM5LTBSRnM5ZAkMwVTh0eWowVU56Y2N4Ry1PV0VLc0VFWjVSU2szVWx1REdNR0dNSmJkQ1dRendpV1BHR1JHTU9pTgZDZD',
     limit: 10,
     template:'<div class="item"><a href="{{link}}" target="_blank"><img title="{{caption}}" src="{{image}}"/></a></div>',
 
